6c0218c5764f14ee56058ab038fe3aad6b0c613a
Mr. Zay <devnddesign@gmail.com>
2026-09-08 14:46:10 -0400

    chore(runtime): export acceptance orchestrator

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ export * from './bootstrap/package-manifest.js';
 export * from './deployment/cloud-run.js';
 export * from './deployment/cloud-run-reliability.js';
 export * from './deployment/cloud-run-reliable-workflow.js';
+export * from './deployment/cloud-run-release-evidence.js';
 export * from './frontend/erase-mask.js';
 export * from './reliability/backend-config.js';
+export * from './runtime/application-runtime-acceptance.js';
 export * from './workflows/cloud-file-workflow.js';
 
 export { createAdaptiveDurationProgressController } from '../Software Engineering & AI Tooling/Frontend Engineering/JavaScript/Aster JavaScript v638.js';
