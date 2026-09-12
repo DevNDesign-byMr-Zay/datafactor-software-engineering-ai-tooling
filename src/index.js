26b5f0e4a8fa7b171a1c3877f76b838353f19ddc
AUREN <AUREN@mr-zay.com>
2026-09-12 15:35:21 -0400

    fix: remove duplicate holographic export

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ export * from './bootstrap/application-bootstrap-readiness.js';
 export * from './bootstrap/package-manifest.js';
 export * from './deployment/cloud-run.js';
 export * from './deployment/cloud-run-reliability.js';
-export * from './deployment/cloud-run-reliability.js';
 export * from './deployment/cloud-run-reliable-workflow.js';
 export * from './deployment/cloud-run-release-evidence.js';
 export * from './frontend/erase-mask.js';
