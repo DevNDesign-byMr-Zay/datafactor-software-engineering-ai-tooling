17456108e8395996666c9c2671e7562627f364f0
AUREN <AUREN@mr-zay.com>
2026-09-12 15:52:14 -0400

    feat: expose HoloCore orchestration API

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ export * from './deployment/cloud-run-reliability.js';
 export * from './deployment/cloud-run-reliable-workflow.js';
 export * from './deployment/cloud-run-release-evidence.js';
 export * from './frontend/erase-mask.js';
+export * from './holo-core/orchestrator.js';
 export * from './holo-core/planner.js';
 export * from './holo-core/scene.js';
 export * from './reliability/backend-config.js';
